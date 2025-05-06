62ac408885d84dadd3f0fc8dadba8e64ea0fe3a9
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2025-05-06 07:08:26 +0300

    app app

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const WebAppScreen(),
+    );
+  }
+}
+
+class WebAppScreen extends StatefulWidget {
+  const WebAppScreen({super.key});
+
+  @override
+  State<WebAppScreen> createState() => _WebAppScreenState();
+}
+
+class _WebAppScreenState extends State<WebAppScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://vr-task-zeta.vercel.app/')); // Replace with your web app URL
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
